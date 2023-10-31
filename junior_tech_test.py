731fc453468585b524b922e398cb8871ccf7ec3f
simon1287 <simonlee1287@hotmail.com>
2023-10-31 12:40:33 +0000

    style(junior_tech_test.py): Flake8 run and formatted to be PEP8 compliant

--- junior_tech_test.py
+++ junior_tech_test.py
@@ -5,6 +5,7 @@ import csv
 # such as math, collections, statistics etc.
 # However, I have no issues implementing these. :-)
 
+
 def read_csv_file(file_path):
     """
     Read a CSV file and return its content as a list of dictionaries.
@@ -22,10 +23,12 @@ def get_unique_teams(data):
     """
     Return a set of unique team names from the provided data.
     """
-        # As sets are unique data elements, duplicates are filtered-out
+    # As sets are unique data elements, duplicates are filtered-out
+
     unique_team_names = set(dict['team_name'] for dict in data)
 
     return unique_team_names
+
 
 def get_most_common_event_type(data):
     """
@@ -51,13 +54,13 @@ def count_event_type_by_team(data, team_name, event_type_name):
     """
     Count the number of events of a specific type for a given team.
     """
-    team_name_filtered_data = filter_by_team(data,team_name)
+    team_name_filtered_data = filter_by_team(data, team_name)
     count = 0
 
     for dict in team_name_filtered_data:
         if dict['event_type_name'] == event_type_name:
             count += 1
-    
+
     return count
 
 
@@ -73,7 +76,7 @@ def average_pass_length_by_team(data, team_name):
         if dict['pass_length'] != '':
             pass_length_total += float(dict['pass_length'])
             count += 1
-    
+
     avg_pass_length = round(pass_length_total / count, 1)
 
     return avg_pass_length
@@ -88,10 +91,10 @@ def filter_players_by_position(data, position_name):
 
     position_name_filter = [dict for dict in data if dict['player_position_name']
                             == position_name]
-    
+
     filtered_player_names = set(dictionary['player_name']
                                 for dictionary in position_name_filter)
-    
+
     return filtered_player_names
 
 
@@ -101,7 +104,7 @@ def count_successful_passes(data):
     """
     successful_pass_filter = [dict for dict in data if dict['event_type_name']
                               == 'Pass' if dict['outcome_name'] == '']
-    
+
     unique_entry_count = len(set(dict['id'] for dict in successful_pass_filter))
 
     return unique_entry_count
@@ -112,7 +115,7 @@ def filter_by_period(data, period):
     Return a list of events that occurred in the provided period (e.g., 1 or 2).
     """
     period_filter = [dict for dict in data if dict['period'] == period]
-        
+
     return period_filter
 
 
